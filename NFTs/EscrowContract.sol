58377fdca4260ffb8aed4331a432bf33b075f8be
Vamshi Adimalla <vamshiadimalla@Vamshis-MacBook-Air.local>
2025-05-11 11:48:07 +0530

    Escrow contact written to save the addresses of the involved parties

---
+++ NFTs/EscrowContract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transderFrom(address to, address from, uint id) external;
+}
+contract EscrowContract {
+    address public nftAddress;
+    address public payable seller;
+    address public inspector;
+    address public lender;
+
+    constructor(address _nftAddress, address payable _seller, address _inspector, address _lender) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+}
